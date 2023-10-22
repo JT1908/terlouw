479bf0a83aa10dca13133541c59f28eacde575eb
JT1908 <82278064+JT1908@users.noreply.github.com>
2023-10-22 14:25:01 +0200

    Add files via upload

---
+++ closeMenu.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const siteNavigation = document.querySelector(".primary-navigation-list");
+const noScroll = document.querySelector("body");
+const mobileLinks = document.querySelectorAll(".nav__link");
+
+mobileLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		menuToggle.setAttribute("aria-expanded", "false");
+		siteNavigation.setAttribute("data-state", "closing");
+		noScroll.classList.remove("no-scroll");
+	});
+});
